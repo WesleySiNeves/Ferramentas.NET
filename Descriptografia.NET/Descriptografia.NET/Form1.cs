25a777e9bdd448aa8ae1690863d9f919f5fa3127
Wesley Neves <wesley.neves@implantainformatica.com.br>
2023-12-04 10:38:57 -0300

    primeiras alterações

---
+++ Descriptografia.NET/Descriptografia.NET/Form1.cs
@@ -0,0 +1,179 @@
+using Descriptografia.NET.Helper;
+using Implanta.Logon.Entity.Acesso;
+using System.Diagnostics;
+using System.Text;
+
+namespace Descriptografia.NET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void BtnDescriptografar_Click(object sender, EventArgs e)
+        {
+            List<PermissoesSistemasEntity> retorno = GetDadosDescriptografado();
+
+            montarResultado(retorno);
+        }
+        private void BtnExportar_Click(object sender, EventArgs e)
+        {
+            List<PermissoesSistemasEntity> retorno = GetDadosDescriptografado();
+
+            montarResultadoHTML(retorno);
+
+        }
+        private void montarResultadoHTML(List<PermissoesSistemasEntity> retorno)
+        {
+            StringBuilder dadosHtml = new StringBuilder();
+
+            // Iniciar a tabela HTML
+            dadosHtml.AppendLine("<html>");
+            dadosHtml.AppendLine("<head>");
+            dadosHtml.AppendLine("<title>Dados</title>");
+            dadosHtml.AppendLine("</head>");
+            dadosHtml.AppendLine("<body>");
+            dadosHtml.AppendLine("<style>");
+            dadosHtml.AppendLine("  table { border-collapse: collapse; width: 100%; }");
+            dadosHtml.AppendLine("  th, td { border: 1px solid black; padding: 8px; text-align: left; }");
+            dadosHtml.AppendLine("  th { background-color: #f0f0f0; }"); // Cor gelo
+            dadosHtml.AppendLine("</style>");
+            dadosHtml.AppendLine("<table>");
+
+            int linha = 1;
+
+            foreach (var item in retorno)
+            {
+                dadosHtml.AppendLine($"<tr><td colspan='4'><strong>{item.NomeSistema} - {item.AcessoString}</strong></td></tr>");
+
+                foreach (var modulo in item.Modulos)
+                {
+                    dadosHtml.AppendLine($"<tr><td colspan='4'>&nbsp;&nbsp;&nbsp;<em>{modulo.Nome} - {modulo.AcessoString}</em></td></tr>");
+
+                    foreach (var funcionalidade in modulo.Funcionalidades)
+                    {
+                        // Adicionar linha para a funcionalidade com a coluna "ordem"
+                        dadosHtml.AppendLine($"<tr><td>{linha}</td><td>&nbsp;&nbsp;&nbsp;&nbsp;{funcionalidade.Nome}</td><td>{funcionalidade.AcessoString}</td><td></td></tr>");
+
+                        // Alternar a cor de fundo para cada linha
+                        dadosHtml.AppendLine(linha % 2 == 0 ? "<style>tr:nth-child(even) { background-color: #ffffff; }</style>" : "<style>tr:nth-child(odd) { background-color: #f0f0f0; }</style>");
+
+                        linha++;
+                    }
+                }
+            }
+
+            // Fechar a tabela e o corpo do HTML
+            dadosHtml.AppendLine("</table>");
+            dadosHtml.AppendLine("</body>");
+            dadosHtml.AppendLine("</html>");
+
+
+
+            // Salvar o HTML em um arquivo (opcional)
+            string diretorioAplicacao = AppDomain.CurrentDomain.BaseDirectory;
+            string caminhoArquivo = Path.Combine(diretorioAplicacao, "exportacao.html");
+
+            File.WriteAllText(caminhoArquivo, dadosHtml.ToString());
+            // Caminho para o executável do Google Chrome
+            string caminhoChrome = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+
+
+            // Abrir o arquivo HTML no navegador padrão
+            // Abrir o arquivo HTML no Google Chrome
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = caminhoChrome,
+                Arguments = caminhoArquivo
+            });
+
+        }
+
+        private void montarResultado(List<PermissoesSistemasEntity> retorno)
+        {
+            StringBuilder dados = new StringBuilder();
+
+            foreach (var item in retorno)
+            {
+                dados.AppendLine($"{item.NomeSistema} - {item.AcessoString}");
+
+                foreach (var modulo in item.Modulos)
+                {
+                    dados.AppendLine($"{modulo.Nome} - {modulo.AcessoString}");
+                    foreach (var funcionalidade in modulo.Funcionalidades)
+                    {
+                        dados.AppendLine($"{funcionalidade.Nome} - {funcionalidade.AcessoString}");
+
+                    }
+                }
+            }
+
+            txtTextoDescriptografado.Text = dados.ToString();
+
+        }
+
+        private void rbPermissaoUsuario_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbPermissaoUsuario.Checked)
+            {
+                rbPermissaoGrupo.Checked = false;
+            }
+        }
+
+        private void rbPermissaoGrupo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbPermissaoGrupo.Checked)
+            {
+                rbPermissaoUsuario.Checked = false;
+            }
+        }
+
+
+
+        private List<PermissoesSistemasEntity> GetDadosDescriptografado()
+        {
+            HelperDescriptografia helper = new HelperDescriptografia();
+
+            var texto = TextoCriptografado;
+
+
+            bool tipoPermissaoUsuario = rbPermissaoUsuario.Checked;
+
+
+            var retorno = helper.Descriptogradar(texto, tipoPermissaoUsuario);
+            return retorno;
+        }
+
+        public string TextoCriptografado
+        {
+            get
+            {
+
+                return TxtTextoCriptografado.Text;
+            }
+
+            set
+            {
+                TxtTextoCriptografado.Text = value;
+            }
+        }
+
+        public string TextoDesCriptografado
+        {
+            get
+            {
+
+                return txtTextoDescriptografado.Text;
+            }
+
+            set
+            {
+                txtTextoDescriptografado.Text = value;
+            }
+        }
+    }
+}
---
+++ Descriptografia.NET/Descriptografia.NET/Helper/HelperDescriptografia.cs
@@ -0,0 +1,115 @@
+﻿using Implanta.Common.Extensions;
+using Implanta.Common.Propriedade.Atributos;
+using Implanta.Common.Util;
+using Implanta.Logon.Entity.Acesso;
+using Implanta.Logon.Entity.Permissao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Descriptografia.NET.Helper
+{
+    public class HelperDescriptografia
+    {
+
+
+        public List<PermissoesSistemasEntity> Descriptogradar(string texto, bool tipoPermissaoUsuario)
+        {
+            List<PermissoesSistemasEntity> permissoes = new List<PermissoesSistemasEntity>();
+
+            SistemasImplantaEntity permissaoSistema;
+
+            if (tipoPermissaoUsuario)
+            {
+                permissaoSistema = texto.FromXML<SistemasImplantaEntity>(true, true);
+            }
+            else
+            {
+                permissaoSistema = texto.FromXML<SistemasImplantaEntity>(true, true);
+            }
+
+            permissoes = GetPermissaoSistema(permissaoSistema);
+        
+
+            return permissoes;
+        }
+
+        private List<PermissoesSistemasEntity> GetPermissaoSistema(SistemasImplantaEntity sistemasImplanta)
+        {
+            List<PermissoesSistemasEntity> retorno = new List<PermissoesSistemasEntity>();
+
+            foreach (var item in ImplantaLogonAttributeHelper.GetSistema(sistemasImplanta))
+            {
+                var sistema = new PermissoesSistemasEntity
+                {
+                    NomeSistema = item.Nome,
+                    NomeInterno = item.NomeInterno,
+                    Descricao = item.Descricao
+                };
+
+                sistema.Modulos = GetModulos(sistemasImplanta, sistema.NomeInterno);
+
+                retorno.Add(sistema);
+            }
+
+            return retorno;
+        }
+
+        private List<PermissoesModulosEntity> GetModulos(SistemasImplantaEntity sistemasImplanta, string sistema)
+        {
+            var retorno = new List<PermissoesModulosEntity>();
+
+            foreach (var modulo in ImplantaLogonAttributeHelper.GetModulo(sistemasImplanta, sistema))
+            {
+                var item = new PermissoesModulosEntity
+                {
+                    Nome = modulo.Nome,
+                    NomeInterno = modulo.NomeInterno,
+                    Descricao = modulo.Descricao
+                };
+
+                item.Funcionalidades = GetFuncionalidades(sistemasImplanta, sistema, item.NomeInterno);
+                retorno.Add(item);
+            }
+
+            return retorno;
+        }
+
+        private List<PermissoesFuncionalidadesEntity> GetFuncionalidades(SistemasImplantaEntity sistemasImplanta, string sistema, string modulo)
+        {
+            var retorno = new List<PermissoesFuncionalidadesEntity>();
+
+            foreach (var funcionalidade in ImplantaLogonAttributeHelper.GetFuncionalidade(sistemasImplanta, sistema, modulo))
+            {
+                var item = new PermissoesFuncionalidadesEntity(funcionalidade.Tipo == TipoPropriedade.CRUD)
+                {
+                    Nome = funcionalidade.Nome,
+                    NomeInterno = funcionalidade.NomeInterno,
+                    Descricao = funcionalidade.Descricao
+                };
+
+                var permissao = ImplantaLogonAttributeHelper.GetPermissao(sistemasImplanta, sistema, modulo, funcionalidade.NomeInterno).FirstOrDefault();
+
+                if (funcionalidade.Tipo == TipoPropriedade.CRUD)
+                {
+                    if (permissao != null)
+                    {
+                        var crud = (CRUD)(permissao.ValorItem ?? new CRUD());
+                        item.AcessoCrud = crud;
+                    }
+                }
+                else
+                {
+                    if (permissao != null) item.AcessoBoleano = (bool)(permissao.ValorItem ?? false);
+                    item.AcessoCrud = new CRUD();
+                }
+
+                retorno.Add(item);
+            }
+
+            return retorno;
+        }
+    }
+}
